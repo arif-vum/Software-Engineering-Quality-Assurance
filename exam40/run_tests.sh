461ca95caff41340f9b144941d85f2c9d4ae2171
arif <just.ar.arif@gmail.com>
2025-04-11 10:48:46 +0300

    question

--- exam40/run_tests.sh
+++ exam40/run_tests.sh
@@ -3,12 +3,14 @@
 
 # Activate Python environment
 source ./selenium_env/bin/activate
+alias pip="./selenium_env/bin/pip"
+alias python="./selenium_env/bin/python"
 
 # Install requirements
 pip install -r requirements.txt
 
 # Run tests with headless Firefox
-python3 -m unittest test_dynamic_controls.py
+python -m unittest test_dynamic_controls.py
 
 
 
