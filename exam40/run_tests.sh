3560ff6049404cc2d70e367aad8b65acd8bb3bb5
arif <just.ar.arif@gmail.com>
2025-04-17 00:53:37 +0300

    shell file

--- exam40/run_tests.sh
+++ exam40/run_tests.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# Jenkins test runner script
-
-# Activate Python environment
-source ./selenium_env/bin/activate
-alias pip="./selenium_env/bin/pip"
-alias python="./selenium_env/bin/python"
-
-# Install requirements
-pip install -r requirements.txt
-
-# Run tests with headless Firefox
-python -m unittest test_dynamic_controls.py
-
-
-
-
-# Deactivate environment
-deactivate
-
+pip install -r requirements.txt --break-system-packages --no-warn-script-location
+python -m xmlrunner test_dynamic_controls.py -o ./test-reports
 
